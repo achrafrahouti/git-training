74aac6444ad0bba37dd78f43905e3bb1e6320055
Achraf Rahouti <arahouti@SMB.SQLI.COM>
2022-03-14 14:38:57 +0100

    Add Human greating

---
+++ Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+
+    public static String greatingHuman() {
+        return "Hello human";
+    }
+    public static void main(String[] args) {
+        System.out.println(greatingHuman());
+    }
+}
