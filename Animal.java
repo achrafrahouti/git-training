5ed8c3ae950ee55de33696c5836bd4a97c1891f4
Achraf Rahouti <arahouti@SMB.SQLI.COM>
2022-03-14 14:46:30 +0100

    Add Cat greating

--- Animal.java
+++ Animal.java
@@ -3,7 +3,16 @@ public class Animal {
     public static String greatingHuman() {
         return "Hello human";
     }
+    public static String greatingCat() {
+        return "Meow!!";
+    }
     public static void main(String[] args) {
-        System.out.println(greatingHuman());
+        String arg1 = args[0];
+        if (arg1.equalsIgnoreCase("cat")) {
+            System.out.println(greatingCat());
+        }
+        else {
+            System.out.println(greatingHuman());
+        }
     }
 }
