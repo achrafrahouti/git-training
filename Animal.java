52a1bdec58141b2baad74996fc894c2d04fc7916
Achraf Rahouti <arahouti@SMB.SQLI.COM>
2022-03-14 14:43:03 +0100

    Add Bird greating

--- Animal.java
+++ Animal.java
@@ -3,7 +3,16 @@ public class Animal {
     public static String greatingHuman() {
         return "Hello human";
     }
+    public static String greatingBird() {
+        return "Tweet!!";
+    }
     public static void main(String[] args) {
-        System.out.println(greatingHuman());
+        String arg1 = args[0];
+        if (arg1.equalsIgnoreCase("bird")) {
+            System.out.println(greatingBird());
+        }
+        else {
+            System.out.println(greatingHuman());
+        }
     }
 }
